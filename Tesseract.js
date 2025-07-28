84951074582d93be997d0ddccbc5464effdff38f
enoksha <sumonbdlg@gmail.com>
2025-07-29 01:39:32 +0600

    Delete Tesseract.js

--- Tesseract.js
+++
@@ -1,8 +0,0 @@
-// Tesseract.js ইন্টিগ্রেশন (স্ক্যান করা PDF-এর জন্য)
-Tesseract.recognize(
-  'path/to/image.png',
-  'ben',
-  { logger: m => console.log(m) }
-).then(({ data: { text } }) => {
-  console.log(text);
-});
